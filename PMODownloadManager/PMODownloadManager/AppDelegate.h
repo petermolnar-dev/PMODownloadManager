d36248ca87f351db5c03265d4f9ee1fccd9e6707
Peter Molnar <peter@petermolnar.hu>
2016-04-10 13:57:03 +0200

    Initial project commit

---
+++ PMODownloadManager/PMODownloadManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PMODownloadManager
+//
+//  Created by Peter Molnar on 10/04/2016.
+//  Copyright © 2016 Peter Molnar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PMODownloadManager/PMODownloadManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PMODownloadManager
+//
+//  Created by Peter Molnar on 10/04/2016.
+//  Copyright © 2016 Peter Molnar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
